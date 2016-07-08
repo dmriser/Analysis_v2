0ae54a25836dc1003b67c237ac5e3cd2900931bf
David Riser <dmriser@Davids-MacBook-Air.local>
2016-07-08 16:08:26 -0400

    going home

--- examples/test.cxx
+++ examples/test.cxx
@@ -31,12 +31,18 @@ int main(int argc, char * argv[])
     // Setup Reader
     int GSIM = 0;                             //! Set to false, not initializing MC banks.
     h22Reader * fReader = new h22Reader(GSIM);
-    vector<string>::iterator it;
-    for (it=OPT.ifiles.begin(); it<OPT.ifiles.end(); it++) { fReader->AddFile(*it); }
+//    vector<string>::iterator it;
+    for (auto it=OPT.ifiles.begin(); it<OPT.ifiles.end(); it++) { fReader->AddFile(*it); }
     fReader->Init();
     
     // Sample event loop.
-    for (int iev=0; iev<OPT.args["N"].arg; iev++) { fReader->GetEntry(iev); h22Event event = fReader->GetEvent(); event.printEvent(); }
+    for (int iev=0; iev<OPT.args["N"].arg; iev++)
+    {
+        fReader->GetEntry(iev);
+        h22Event event = fReader->GetEvent();
+        event.printEvent();
+        
+    }
     
     std::cout << " Done! " << std::endl;
     

d58c81b748fc6ceee2334829e1aef71681728c32
David Riser <dmriser@Davids-MacBook-Air.local>
2016-07-08 15:54:58 -0400

    Working locally, added scripts to setup.

--- examples/test.cxx
+++ examples/test.cxx
@@ -1,42 +1,45 @@
 //////////////////////////////////////
 /*
-
-  David Riser, University of Connecticut 
-
-  test.C -> test the usage of libraries 
-
-*/
+ 
+ David Riser, University of Connecticut
+ 
+ test.C -> test the usage of libraries
+ 
+ */
 //////////////////////////////////////
 
-// c++ includes 
+// c++ includes
 #include <iostream>
 #include <vector>
 
-// my includes 
+// my includes
 #include "h22Event.h"
 #include "h22Option.h"
 #include "h22Reader.h"
 
+// root includes
+#include "TVector3.h"
+
 int main(int argc, char * argv[])
 {
-
-
-  // Setup Options 
-  h22Options OPT;
-  OPT.set(argc,argv);
-
-  // Setup Reader 
-  int GSIM = 0;                             //! Set to false, not initializing MC banks.
-  h22Reader * fReader = new h22Reader(GSIM);
-  vector<string>::iterator it;
-  for (it=OPT.ifiles.begin(); it<OPT.ifiles.end(); it++) { fReader->AddFile(*it); }
-  fReader->Init();
-
-  // Sample event loop.
-  //  for (int iev=0; iev<OPT.args["N"].arg; iev++) { fReader->GetEntry(iev); h22Event event = fReader->GetEvent(); event.printEvent(); }
-
-  std::cout << " Done! " << std::endl;
-  
-
-  return 0;
+    
+    
+    // Setup Options
+    h22Options OPT;
+    OPT.set(argc,argv);
+    
+    // Setup Reader
+    int GSIM = 0;                             //! Set to false, not initializing MC banks.
+    h22Reader * fReader = new h22Reader(GSIM);
+    vector<string>::iterator it;
+    for (it=OPT.ifiles.begin(); it<OPT.ifiles.end(); it++) { fReader->AddFile(*it); }
+    fReader->Init();
+    
+    // Sample event loop.
+    for (int iev=0; iev<OPT.args["N"].arg; iev++) { fReader->GetEntry(iev); h22Event event = fReader->GetEvent(); event.printEvent(); }
+    
+    std::cout << " Done! " << std::endl;
+    
+    
+    return 0;
 }
--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
-#/bin/tcsh 
+#/bin/bash
 
 # compiling libraries 
 scons 
 
-set NLIBS    = `ls -l lib/*.so | wc -l`
+set NLIBS    = `ls -l lib/*.dylib | wc -l`
 set NHEADERS = `ls -l */*.h | wc -l`
 
 # moving to dest
-cp lib/*.so /u/home/dmriser/lib/
-cp */*.h /u/home/dmriser/include/
+cp lib/*.dylib /usr/local/lib/
+cp */*.h /usr/local/include/
 
 echo " >> Finished! Installed $NLIBS libraries and $NHEADERS header files! "
